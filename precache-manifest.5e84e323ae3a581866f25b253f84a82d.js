46ddd97c6c28f26a9428c2dbb2c81a6de73a3540
Mike Ferrete <michael.ferrete@gmail.com>
2019-04-05 16:49:24 -0400

    Updates

---
+++ precache-manifest.5e84e323ae3a581866f25b253f84a82d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11b97adfa0efa69dba6a",
+    "url": "/react-tutorial/static/css/main.9a758284.chunk.css"
+  },
+  {
+    "revision": "11b97adfa0efa69dba6a",
+    "url": "/react-tutorial/static/js/main.bbf49cb3.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-tutorial/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "52466e9b395e4e8b27c9e5cb6337443e",
+    "url": "/react-tutorial/index.html"
+  }
+];
